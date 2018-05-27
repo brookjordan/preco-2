0786f8996871f0b465a55a58bac904b3c280112d
Brook Jordan <brkjrdn@gmail.com>
2018-05-27 17:30:26 +0800

    SVG optimisations

--- app/components/preco-logo-definition.js
+++ app/components/preco-logo-definition.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'defs',
-  classNames: ['svg-icon-definitions'],
+  classNames: ['preco-logo-definition'],
 });
--- app/components/preco-logo.js
+++ app/components/preco-logo.js
@@ -6,4 +6,8 @@ export default Component.extend({
   tagName: 'svg',
   xmlns: "http://www.w3.org/2000/svg",
   viewBox: "0 0 98 98",
+  theme: 'color',
+})
+.reopenClass({
+  positionalParams: ['theme'],
 });
